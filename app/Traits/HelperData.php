726d5a5162a39baae77b590d846e4b00c94802fe
vicbat <vicbat@ukr.net>
2020-06-04 22:57:55 +0300

    fix add income

--- app/Traits/HelperData.php
+++ app/Traits/HelperData.php
@@ -2,6 +2,7 @@
 
 namespace App\Traits;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Http\FormRequest;
 
 trait HelperData
@@ -9,6 +10,10 @@ trait HelperData
     public function prepareData(FormRequest $request)
     {
         $data = collect($request->validated())->except(key($request->route()->parameters()))->toArray();
+
+        if ($request->get('date')) {
+            $data['date'] = Carbon::parse($data['date']);
+        }
 
         return $data;
     }
